403bd7cefffb0a8f83afa3d941984bc5a8ee4614
jbdres <julian.betov@gmail.com>
2025-04-04 18:11:57 -0500

    Create Product, Category and Image entities

---
+++ src/main/java/com/DevCourses/ShoppingCartBackend/model/Image.java
@@ -0,0 +1,25 @@
+package com.DevCourses.ShoppingCartBackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Blob;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+}
