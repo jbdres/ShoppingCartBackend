eed1edf0efa873d8ce5bb344a86a3765dcfb51db
Julian Bermudez <julian.betov@gmail.com>
2025-04-22 20:05:50 -0500

    feat: add Category entity with JPA annotations

---
+++ src/main/java/com/devpractice/shoppingcartbackend/model/Category.java
@@ -0,0 +1,22 @@
+package com.devpractice.shoppingcartbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String name;
+
+}
