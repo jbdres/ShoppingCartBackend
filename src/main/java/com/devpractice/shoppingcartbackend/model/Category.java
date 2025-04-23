4791cf01531f1d06e96cbb31511143e56a66fbe8
Julian Bermudez <julian.betov@gmail.com>
2025-04-22 20:51:42 -0500

    feat: add validation constraints to Category entity

--- src/main/java/com/devpractice/shoppingcartbackend/model/Category.java
+++ src/main/java/com/devpractice/shoppingcartbackend/model/Category.java
@@ -1,6 +1,7 @@
 package com.devpractice.shoppingcartbackend.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +17,11 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
     Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Column(name = "name", nullable = false, unique = true, length = 100)
     String name;
 
 }
