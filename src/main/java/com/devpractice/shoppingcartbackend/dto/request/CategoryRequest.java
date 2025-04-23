5f3b1e810ea6d6bff821cc94a1dedec06a182a8b
Julian Bermudez <julian.betov@gmail.com>
2025-04-22 20:30:40 -0500

    feat: add validation annotations to CategoryRequest DTO

--- src/main/java/com/devpractice/shoppingcartbackend/dto/request/CategoryRequest.java
+++ src/main/java/com/devpractice/shoppingcartbackend/dto/request/CategoryRequest.java
@@ -1,7 +1,12 @@
 package com.devpractice.shoppingcartbackend.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CategoryRequest(
 
+        @NotBlank(message = "Name is required")
+        @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
         String name
 
 ) {
