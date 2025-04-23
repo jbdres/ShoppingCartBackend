22ef49f90206830ff4eab81e231c7467a05b5dbd
Julian Bermudez <julian.betov@gmail.com>
2025-04-23 14:20:55 -0500

    feat: enhance CategoryServiceImp to handle category creation with conflict and database error handling

--- src/main/java/com/devpractice/shoppingcartbackend/service/imp/CategoryServiceImp.java
+++ src/main/java/com/devpractice/shoppingcartbackend/service/imp/CategoryServiceImp.java
@@ -2,17 +2,37 @@ package com.devpractice.shoppingcartbackend.service.imp;
 
 import com.devpractice.shoppingcartbackend.dto.request.CategoryRequest;
 import com.devpractice.shoppingcartbackend.dto.response.CategoryResponse;
+import com.devpractice.shoppingcartbackend.exception.DatabaseException;
+import com.devpractice.shoppingcartbackend.exception.ResourceConflictException;
+import com.devpractice.shoppingcartbackend.mapper.CategoryMapper;
+import com.devpractice.shoppingcartbackend.model.Category;
+import com.devpractice.shoppingcartbackend.repository.CategoryRepository;
 import com.devpractice.shoppingcartbackend.service.interfaces.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryServiceImp implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
 
     @Override
     public CategoryResponse createCategory(CategoryRequest categoryRequest) {
-        return null;
+        if (categoryRepository.existsByName(categoryRequest.name())) {
+            throw new ResourceConflictException("Category with name " + categoryRequest.name() + " already exists");
+        }
+        try {
+            Category category = categoryMapper.toEntity(categoryRequest);
+            Category savedCategory = categoryRepository.save(category);
+            return categoryMapper.toResponse(savedCategory);
+        } catch (DataAccessException e) {
+            throw new DatabaseException("Error while saving category to the database");
+        }
     }
 
     @Override
