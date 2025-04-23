d5b3282575cf7cc766a29616dee386465544282d
Julian Bermudez <julian.betov@gmail.com>
2025-04-23 12:15:48 -0500

    feat: create BusinessException class for custom error handling

---
+++ src/main/java/com/devpractice/shoppingcartbackend/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.devpractice.shoppingcartbackend.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final String errorCode;
+
+    public BusinessException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+}
