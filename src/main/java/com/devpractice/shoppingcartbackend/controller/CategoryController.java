c8c7664e99e0ad684104a296ac7ad5e55dcc53b5
Julian Bermudez <julian.betov@gmail.com>
2025-04-23 15:38:30 -0500

    feat: add getAllCategories endpoint to retrieve all categories

--- src/main/java/com/devpractice/shoppingcartbackend/controller/CategoryController.java
+++ src/main/java/com/devpractice/shoppingcartbackend/controller/CategoryController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/categories")
 @RequiredArgsConstructor
@@ -26,6 +28,12 @@ public class CategoryController {
     public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable Long id) {
         CategoryResponse categoryResponse = categoryService.getCategoryById(id);
         return ResponseEntity.ok(categoryResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        List<CategoryResponse> categoryResponses = categoryService.getAllCategories();
+        return ResponseEntity.ok(categoryResponses);
     }
 
 }
