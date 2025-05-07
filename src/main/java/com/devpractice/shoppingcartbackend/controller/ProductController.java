4f8825b97b2bbabbe1027a58f147abe03240eb05
Julian Bermudez <julian.betov@gmail.com>
2025-05-07 12:23:22 -0500

    feat(controller): add createProduct endpoint to ProductController

--- src/main/java/com/devpractice/shoppingcartbackend/controller/ProductController.java
+++ src/main/java/com/devpractice/shoppingcartbackend/controller/ProductController.java
@@ -1,7 +1,14 @@
 package com.devpractice.shoppingcartbackend.controller;
 
+import com.devpractice.shoppingcartbackend.dto.request.ProductRequest;
+import com.devpractice.shoppingcartbackend.dto.response.ProductResponse;
 import com.devpractice.shoppingcartbackend.service.interfaces.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequest productRequest) {
+        ProductResponse productResponse = productService.createProduct(productRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
+    }
 
 }
