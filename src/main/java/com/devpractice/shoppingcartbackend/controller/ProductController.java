02eaad89af44a2c6f9f842d1bf847525d3c19847
Julian Bermudez <julian.betov@gmail.com>
2025-05-07 11:44:49 -0500

    feat(controller): add ProductController to handle product-related requests

---
+++ src/main/java/com/devpractice/shoppingcartbackend/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.devpractice.shoppingcartbackend.controller;
+
+import com.devpractice.shoppingcartbackend.service.interfaces.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+}
